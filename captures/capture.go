1fac4f6368a806e4200903ae5367702efb5abf03
Freddy Rondon <freddy.rondon@mercadolibre.cl>
2017-10-06 19:12:34 -0300

    feat: add capture basic constructor

--- captures/capture.go
+++ captures/capture.go
@@ -9,6 +9,11 @@ import (
 // Capture is the representation of data sample of any kind taken at a specific time and location.
 type Capture struct {
 	Point     *geocoding.Point
-	Timestamp time.Duration
+	Timestamp time.Time
 	Payload   interface{}
+}
+
+// NewCapture returns a new pointer to a Capture composed of the passed Point, Time and payload
+func NewCapture(point *geocoding.Point, timestamp time.Time, payload interface{}) *Capture {
+	return &Capture{point, timestamp, payload}
 }
---
+++ captures/capture_test.go
@@ -0,0 +1,21 @@
+package captures_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/ifreddyrondon/gocapture/captures"
+	"github.com/ifreddyrondon/gocapture/geocoding"
+)
+
+func TestNewCapture(t *testing.T) {
+	point, _ := geocoding.NewPoint(1, 2)
+	timestamp := time.Now()
+	var payload interface{}
+
+	result := captures.NewCapture(point, timestamp, payload)
+
+	if result == nil {
+		t.Errorf("Expected capture not to nil. Got '%v'", result)
+	}
+}
