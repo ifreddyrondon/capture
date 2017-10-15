1b7c2e89ea2e281d6e06067b72e668e8ac534f08
Freddy Rondon <ifreddyrondon@gmail.com>
2017-10-15 14:14:28 -0300

    feat(capture): add UnmarshalJSON method to capture

--- captures/capture.go
+++ captures/capture.go
@@ -1,8 +1,6 @@
 package captures
 
 import (
-	"log"
-
 	"github.com/ifreddyrondon/gocapture/geocoding"
 )
 
@@ -16,4 +14,21 @@ type Capture struct {
 // NewCapture returns a new pointer to a Capture composed of the passed Point, Time and payload
 func NewCapture(point *geocoding.Point, timestamp *Date, payload interface{}) *Capture {
 	return &Capture{point, timestamp, payload}
+}
+
+// UnmarshalJSON decodes the capture from a JSON body.
+// Throws an error if the body of the date cannot be interpreted by the JSON body.
+// Implements the json.Unmarshaler Interface
+func (c *Capture) UnmarshalJSON(data []byte) error {
+	p := geocoding.Point{}
+	if err := p.UnmarshalJSON(data); err != nil {
+		return err
+	}
+
+	date := Date{}
+	date.UnmarshalJSON(data)
+
+	capture := NewCapture(&p, &date, "")
+	*c = *capture
+	return nil
 }
--- captures/capture_test.go
+++ captures/capture_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 	"time"
 
+	"os"
+
 	"github.com/ifreddyrondon/gocapture/captures"
 	"github.com/ifreddyrondon/gocapture/geocoding"
 )
@@ -18,4 +20,74 @@ func TestNewCapture(t *testing.T) {
 	if result == nil {
 		t.Errorf("Expected capture not to nil. Got '%v'", result)
 	}
+}
+
+func TestCaptureUnmarshalJSON(t *testing.T) {
+	defer os.Setenv("TZ", os.Getenv("TZ"))
+	os.Setenv("TZ", "UTC")
+
+	tt := []struct {
+		name      string
+		payload   []byte
+		result    *captures.Capture
+		resultErr error
+	}{
+		{
+			"valid point with given date",
+			[]byte(`{"lat": 1, "lng": 1, "date": "1989-12-26T06:01:00.00Z"}`),
+			getCapture(1, 1, "1989-12-26T06:01:00.00Z", ""),
+			nil,
+		},
+		{
+			"invalid point",
+			[]byte(`{"lat": -91, "lng": 1, "date": "1989-12-26T06:01:00.00Z"}`),
+			nil,
+			geocoding.PointRangeLATError,
+		},
+		{
+			"missing point lat",
+			[]byte(`{"lng": 1, "date": "1989-12-26T06:01:00.00Z"}`),
+			nil,
+			geocoding.PointMissingLATError,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			result := captures.Capture{}
+			resultError := result.UnmarshalJSON(tc.payload)
+
+			if resultError != tc.resultErr {
+				t.Errorf("Expected get the error '%v'. Got '%v'", tc.resultErr, resultError)
+			}
+
+			// if result expected an error do not check for internal attrs
+			if tc.resultErr != nil {
+				return
+			}
+
+			if result.Point.Lat != tc.result.Point.Lat {
+				t.Errorf("Expected Lat of capture to be '%v'. Got '%v'", tc.result.Point.Lat, result.Point.Lat)
+			}
+
+			if result.Point.Lng != tc.result.Point.Lng {
+				t.Errorf("Expected Lng of capture to be '%v'. Got '%v'", tc.result.Point.Lng, result.Point.Lng)
+			}
+
+			if result.Date.Timestamp.String() != tc.result.Date.Timestamp.String() {
+				t.Errorf(
+					"Expected Date of capture to be '%v'. Got '%v'",
+					tc.result.Date.Timestamp.String(),
+					result.Date.Timestamp.String())
+			}
+		})
+	}
+}
+
+func getCapture(lat, lng float64, date string, payload interface{}) *captures.Capture {
+	p, _ := geocoding.NewPoint(lat, lng)
+	parsedDate, _ := time.Parse(time.RFC3339, date)
+	timestamp := captures.NewDate(parsedDate)
+
+	return captures.NewCapture(p, timestamp, payload)
 }
