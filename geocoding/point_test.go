689507eb91465770a6cdc77e098d31944417383c
Freddy Rondon <ifreddyrondon@gmail.com>
2018-02-06 21:27:32 -0300

    test: refactor point test with assert library

--- geocoding/point_test.go
+++ geocoding/point_test.go
@@ -5,21 +5,40 @@ import (
 
 	"github.com/ifreddyrondon/gocapture/geocoding"
 
-	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestNewPoint(t *testing.T) {
+func TestNewPointSuccess(t *testing.T) {
 	tt := []struct {
 		name     string
 		lat, lng float64
-		err      error
 	}{
-		{"valid with lng upper limit", 75, 180, nil},
-		{"valid with lat upper limit", 90, -147.45, nil},
-		{"valid with decimals", 77.11112223331, 149.99999999, nil},
-		{"valid both upper limits", 90, 180, nil},
-		{"valid both lower limits", -90.00000, -180.0000, nil},
-		{"valid with just point decimal", 90., 180., nil},
+		{"valid with lng upper limit", 75, 180},
+		{"valid with lat upper limit", 90, -147.45},
+		{"valid with decimals", 77.11112223331, 149.99999999},
+		{"valid both upper limits", 90, 180},
+		{"valid both lower limits", -90.00000, -180.0000},
+		{"valid with just point decimal", 90., 180.},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := geocoding.NewPoint(tc.lat, tc.lng)
+			require.Nil(t, err)
+			require.NotNil(t, result)
+			assert.Equal(t, tc.lng, result.Lng)
+			assert.Equal(t, tc.lat, result.Lat)
+		})
+	}
+}
+
+func TestNewPointFailure(t *testing.T) {
+	tt := []struct {
+		name     string
+		lat, lng float64
+		expected error
+	}{
 		{"invalid lat > 90", 95, 280, geocoding.ErrorLATRange},
 		{"invalid lat < -95", -95, 280, geocoding.ErrorLATRange},
 		{"invalid lng > 180", 75, 280, geocoding.ErrorLONRange},
@@ -29,25 +48,9 @@ func TestNewPoint(t *testing.T) {
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			p, err := geocoding.NewPoint(tc.lat, tc.lng)
-
-			if tc.err == nil {
-				if p == nil {
-					t.Errorf("Expected point not to nil. Got '%v'", p)
-				}
-
-				if p.Lat != tc.lat {
-					t.Errorf("Expected result lat point to be '%v'. Got '%v'", tc.lat, p.Lat)
-				}
-
-				if p.Lng != tc.lng {
-					t.Errorf("Expected result lng point to be '%v'. Got '%v'", tc.lng, p.Lng)
-				}
-			} else {
-				if err == nil {
-					t.Errorf("Expected error to be %v. Got '%v'", tc.err, p)
-				}
-			}
+			point, err := geocoding.NewPoint(tc.lat, tc.lng)
+			require.Nil(t, point)
+			assert.Equal(t, tc.expected, err)
 		})
 	}
 }
@@ -55,82 +58,74 @@ func TestNewPoint(t *testing.T) {
 func TestPointMarshalJSON(t *testing.T) {
 	p := getPoint(1, 1)
 	expected := `{"lat":1,"lng":1}`
-	result, _ := p.MarshalJSON()
-
-	if expected != string(result) {
-		t.Errorf("Expected Marshal result to be '%v'. Got '%v'", expected, string(result))
-	}
+	result, err := p.MarshalJSON()
+	require.Nil(t, err)
+	assert.Equal(t, expected, string(result))
 }
 
-func TestUnmarshalJSON(t *testing.T) {
+func TestUnmarshalJSONSuccess(t *testing.T) {
 	tt := []struct {
-		name        string
-		payload     []byte
-		resultPoint *geocoding.Point
-		resultError error
+		name     string
+		payload  []byte
+		expected *geocoding.Point
 	}{
 		{
 			"valid lat and lng",
-			pointToBytes(40.7486, -73.9864),
+			[]byte(`{"lat":40.7486, "lng":-73.9864}`),
 			getPoint(40.7486, -73.9864),
-			nil,
 		},
 		{
 			"valid with latitude and longitude",
 			[]byte(`{"latitude":1, "longitude":1}`),
 			getPoint(1, 1),
-			nil,
 		},
 		{
 			"valid mixed latitude and lng",
 			[]byte(`{"latitude":1, "lng":1}`),
 			getPoint(1, 1),
-			nil,
 		},
 		{
 			"valid mixed lat and longitude",
 			[]byte(`{"lat":1, "longitude":1}`),
 			getPoint(1, 1),
-			nil,
 		},
-		{"invalid empty json", []byte("{}"), nil, geocoding.ErrorLATMissing},
-		{"invalid lat", pointToBytes(100, 1), nil, geocoding.ErrorLATRange},
-		{"invalid lng", pointToBytes(1, 190), nil, geocoding.ErrorLONRange},
-		{"invalid json", []byte("`"), nil, geocoding.ErrorUnmarshalPoint},
-		{"missing lat", []byte(`{"lng":1}`), nil, geocoding.ErrorLATMissing},
-		{"missing lng", []byte(`{"lat":1}`), nil, geocoding.ErrorLNGMissing},
-		{"missing latitude", []byte(`{"longitude":1}`), nil, geocoding.ErrorLATMissing},
-		{"missing longitude", []byte(`{"latitude":1}`), nil, geocoding.ErrorLNGMissing},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			resultPoint := geocoding.Point{}
-			resultError := resultPoint.UnmarshalJSON(tc.payload)
-
-			if resultError != tc.resultError {
-				t.Errorf("Expected get the error '%v'. Got '%v'", tc.resultError, resultError)
-			}
-
-			// if result expected an error do not check for internal attrs
-			if tc.resultError != nil {
-				return
-			}
-
-			if resultPoint.Lat != tc.resultPoint.Lat {
-				t.Errorf("Expected result lat point to be '%v'. Got '%v'", tc.resultPoint.Lat, resultPoint.Lat)
-			}
-
-			if resultPoint.Lng != tc.resultPoint.Lng {
-				t.Errorf("Expected result lng point to be '%v'. Got '%v'", tc.resultPoint.Lng, resultPoint.Lng)
-			}
+			result := geocoding.Point{}
+			err := result.UnmarshalJSON(tc.payload)
+			require.Nil(t, err)
+			require.NotNil(t, result)
+			assert.Equal(t, tc.expected.Lng, result.Lng)
+			assert.Equal(t, tc.expected.Lat, result.Lat)
 		})
 	}
 }
 
-func pointToBytes(lat, lng float64) []byte {
-	res := fmt.Sprintf(`{"lat":%v, "lng":%v}`, lat, lng)
-	return []byte(res)
+func TestUnmarshalJSONFailure(t *testing.T) {
+	tt := []struct {
+		name     string
+		payload  []byte
+		expected error
+	}{
+		{"invalid empty json", []byte("{}"), geocoding.ErrorLATMissing},
+		{"invalid lat", []byte(`{"lat":100, "lng": 1}`), geocoding.ErrorLATRange},
+		{"invalid lng", []byte(`{"lat":1, "lng": 190}`), geocoding.ErrorLONRange},
+		{"invalid json", []byte("`"), geocoding.ErrorUnmarshalPoint},
+		{"missing lat", []byte(`{"lng":1}`), geocoding.ErrorLATMissing},
+		{"missing lng", []byte(`{"lat":1}`), geocoding.ErrorLNGMissing},
+		{"missing latitude", []byte(`{"longitude":1}`), geocoding.ErrorLATMissing},
+		{"missing longitude", []byte(`{"latitude":1}`), geocoding.ErrorLNGMissing},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			p := geocoding.Point{}
+			err := p.UnmarshalJSON(tc.payload)
+			assert.Equal(t, tc.expected, err)
+		})
+	}
 }
 
 func getPoint(lat, lng float64) *geocoding.Point {
