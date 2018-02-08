7bbc0986759406c13ac3636541ff2014be7a0e3f
Freddy Rondon <ifreddyrondon@gmail.com>
2018-02-07 22:46:50 -0300

    test: refactor payload.ArrayNumberPayload test with assert library

--- payload/array_number_test.go
+++ payload/array_number_test.go
@@ -6,55 +6,67 @@ import (
 	"encoding/json"
 
 	"github.com/ifreddyrondon/gocapture/payload"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestArrayNumberPayloadUnmarshalJSON(t *testing.T) {
+func TestArrayNumberPayloadUnmarshalJSONSuccess(t *testing.T) {
 	tt := []struct {
 		name     string
 		payload  []byte
 		expected payload.ArrayNumberPayload
-		err      error
 	}{
 		{
 			"unmarshal with cap",
 			[]byte(`{"cap": [-78.75, -80.5, -73.75, -70.75, -72]}`),
 			[]float64{-78.75, -80.5, -73.75, -70.75, -72},
-			nil,
 		},
 		{
 			"unmarshal with captures",
 			[]byte(`{"captures": [-78.75, -80.5, -73.75, -70.75, -72]}`),
 			[]float64{-78.75, -80.5, -73.75, -70.75, -72},
-			nil,
 		},
 		{
 			"unmarshal with data",
 			[]byte(`{"data": [-78.75, -80.5, -73.75, -70.75, -72]}`),
 			[]float64{-78.75, -80.5, -73.75, -70.75, -72},
-			nil,
 		},
 		{
 			"unmarshal with payload",
 			[]byte(`{"payload": [-78.75, -80.5, -73.75, -70.75, -72]}`),
 			[]float64{-78.75, -80.5, -73.75, -70.75, -72},
-			nil,
 		},
 		{
 			"unmarshal empty payload",
 			[]byte(`{"payload": []}`),
 			[]float64{},
-			nil,
 		},
 		{
 			"unmarshal empty body",
 			[]byte(`{}`),
 			nil,
-			nil,
 		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			result := payload.ArrayNumberPayload{}
+			err := result.UnmarshalJSON(tc.payload)
+			require.Nil(t, err)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestArrayNumberPayloadUnmarshalJSON(t *testing.T) {
+	tt := []struct {
+		name     string
+		payload  []byte
+		expected error
+	}{
 		{
 			"unmarshal error",
 			[]byte(`'`),
-			nil,
 			payload.ErrorUnmarshalPayload,
 		},
 	}
@@ -63,25 +75,7 @@ func TestArrayNumberPayloadUnmarshalJSON(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			result := payload.ArrayNumberPayload{}
 			err := result.UnmarshalJSON(tc.payload)
-
-			if tc.err != err {
-				t.Errorf("Expected get the error '%v'. Got '%v'", tc.err, err)
-			}
-
-			// if result expected an error do not check for internal attrs
-			if tc.err != nil {
-				return
-			}
-
-			if len(result) != len(tc.expected) {
-				t.Errorf("Expected payload to be '%v'. Got '%v'", len(tc.expected), len(result))
-			}
-
-			for i, v := range tc.expected {
-				if v != result[i] {
-					t.Fatalf("Expected payload at index %v to be '%v'. Got '%v'", i, v, result[i])
-				}
-			}
+			assert.Equal(t, tc.expected, err)
 		})
 	}
 }
@@ -101,11 +95,9 @@ func TestArrayNumberPayloadMarshalJSON(t *testing.T) {
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			result, _ := json.Marshal(tc.payload)
-
-			if tc.expected != string(result) {
-				t.Errorf("Expected Marshal result to be '%v'. Got '%v'", tc.expected, string(result))
-			}
+			result, err := json.Marshal(tc.payload)
+			require.Nil(t, err)
+			assert.Equal(t, tc.expected, string(result))
 		})
 	}
 }
