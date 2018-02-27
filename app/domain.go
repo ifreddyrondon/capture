80b71dbd2fcb943f6aa6fcc89c61d20bf68c47c4
Freddy Rondon <ifreddyrondon@gmail.com>
2018-02-27 05:53:49 -0300

    doc: add Router doc

--- app/domain.go
+++ app/domain.go
@@ -4,6 +4,9 @@ import (
 	"github.com/go-chi/chi"
 )
 
+// Router is the interface implemented by the controllers.
+// It allows the auto attach of the Router() (http.Handler)
+// as a subrouter along a routing Pattern()
 type Router interface {
 	Pattern() string
 	Router() chi.Router
