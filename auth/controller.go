90bccaa64b2824eb2b489926c4d24da8db5b7e04
Freddy Rondon <ifreddyrondon@gmail.com>
2018-05-07 18:19:45 -0300

    feat(auth): authenticate from controller

--- auth/controller.go
+++ auth/controller.go
@@ -4,13 +4,14 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/ifreddyrondon/bastion/render"
-
 	"github.com/ifreddyrondon/bastion"
+	"github.com/ifreddyrondon/bastion/render"
+	bastionJSON "github.com/ifreddyrondon/bastion/render/json"
 )
 
 // Controller handler the auth routes
 type Controller struct {
+	Service
 	render.Render
 }
 
@@ -29,4 +30,21 @@ func (c *Controller) login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	u, err := c.Service.Authenticate(&credentials)
+	if err != nil {
+		if err == errInvalidCredentials {
+			httpErr := bastionJSON.HTTPError{
+				Status:  http.StatusUnauthorized,
+				Errors:  http.StatusText(http.StatusUnauthorized),
+				Message: err.Error(),
+			}
+			_ = c.Render(w).Response(http.StatusUnauthorized, httpErr)
+			return
+		}
+
+		_ = c.Render(w).InternalServerError(err)
+		return
+	}
+
+	_ = c.Render(w).Created(u)
 }
