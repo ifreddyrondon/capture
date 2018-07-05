f5e49045e3867f6a6219793468b3d204f85d7ed1
Freddy Rondon <ifreddyrondon@gmail.com>
2018-07-04 23:09:31 -0400

    feat: add paging marshal

--- app/listing/paging/paging.go
+++ app/listing/paging/paging.go
@@ -2,8 +2,8 @@ package paging
 
 // Paging struct allows to do pagination into a collection.
 type Paging struct {
-	MaxAllowedLimit int
-	Limit           int
-	Offset          int64
-	Total           int64
+	MaxAllowedLimit int   `json:"max_allowed_limit"`
+	Limit           int   `json:"limit"`
+	Offset          int64 `json:"offset"`
+	Total           int64 `json:"total,omitempty"`
 }
