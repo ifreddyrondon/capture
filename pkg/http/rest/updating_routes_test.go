1d5898705af5de3fcf2dcd4f61ecc0ef80351257
Freddy Rondon <ifreddyrondon@gmail.com>
2019-01-08 10:12:36 -0300

    test: add test to updating capture

---
+++ pkg/http/rest/updating_routes_test.go
@@ -0,0 +1,160 @@
+package rest_test
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/ifreddyrondon/bastion"
+	"github.com/ifreddyrondon/capture/pkg/domain"
+	"github.com/ifreddyrondon/capture/pkg/http/rest"
+	"github.com/ifreddyrondon/capture/pkg/updating"
+	"gopkg.in/src-d/go-kallax.v1"
+)
+
+type mockUpdatingCaptureService struct {
+	err error
+}
+
+func (m *mockUpdatingCaptureService) Update(updating.Capture, *domain.Capture) error {
+	return m.err
+}
+
+func setupUpdatingCaptureHandler(s updating.CaptureService, m func(http.Handler) http.Handler) *bastion.Bastion {
+	app := bastion.New()
+	app.APIRouter.Use(m)
+	app.APIRouter.Put("/", rest.UpdatingCapture(s))
+	return app
+}
+
+func TestUpdatingCaptureSuccess(t *testing.T) {
+	t.Parallel()
+
+	captureID := kallax.NewULID()
+	capt := &domain.Capture{
+		ID: captureID,
+		Payload: domain.Payload{
+			domain.Metric{Name: "power", Value: []float64{-70.0, -100.1, 3.1}},
+		},
+		Timestamp: s2t("1989-12-26T06:01:00.00Z"),
+		Location:  &domain.Point{LAT: f2P(1), LNG: f2P(1), Elevation: f2P(1)},
+		Tags:      []string{"at night"},
+	}
+
+	body := map[string]interface{}{
+		"location": map[string]float64{
+			"lat":       10,
+			"lng":       *capt.Location.LNG,
+			"elevation": *capt.Location.Elevation,
+		},
+	}
+
+	expected := map[string]interface{}{
+		"id":        captureID.String(),
+		"payload":   []map[string]interface{}{{"name": "power", "value": []interface{}{-70.0, -100.1, 3.1}}},
+		"timestamp": "1989-12-26T06:01:00Z",
+		"location":  map[string]float64{"lat": 1, "lng": 1, "elevation": 1},
+		"tags":      []string{"at night"},
+	}
+
+	s := &mockUpdatingCaptureService{}
+	app := setupUpdatingCaptureHandler(s, withCaptureMiddle(capt))
+	e := bastion.Tester(t, app)
+
+	e.PUT("/").WithJSON(body).Expect().
+		Status(http.StatusOK).
+		JSON().Object().
+		ContainsKey("id").ValueEqual("id", expected["id"]).
+		ContainsKey("location").ValueEqual("location", expected["location"]).
+		ContainsKey("timestamp").ValueEqual("timestamp", expected["timestamp"]).
+		ContainsKey("payload").ValueEqual("payload", expected["payload"]).
+		ContainsKey("tags").ValueEqual("tags", expected["tags"]).
+		ContainsKey("createdAt").NotEmpty().
+		ContainsKey("updatedAt").NotEmpty().
+		Raw()
+}
+
+func TestUpdatingCaptureFailsGettingCapture(t *testing.T) {
+	t.Parallel()
+
+	app := setupUpdatingCaptureHandler(&mockUpdatingCaptureService{}, withCaptureMiddle(nil))
+
+	body := map[string]interface{}{
+		"location": map[string]float64{"lat": 10, "lng": 1, "elevation": 1},
+	}
+	response := map[string]interface{}{
+		"status":  500.0,
+		"error":   "Internal Server Error",
+		"message": "looks like something went wrong",
+	}
+
+	e := bastion.Tester(t, app)
+	e.PUT("/").WithJSON(body).Expect().
+		Status(http.StatusInternalServerError).
+		JSON().Object().Equal(response)
+}
+
+func TestUpdatingCaptureFailsUpdating(t *testing.T) {
+	t.Parallel()
+	s := &mockUpdatingCaptureService{err: errors.New("test")}
+	app := setupUpdatingCaptureHandler(s, withCaptureMiddle(defaultCapture))
+
+	body := map[string]interface{}{
+		"location": map[string]float64{"lat": 10, "lng": 1, "elevation": 1},
+	}
+	response := map[string]interface{}{
+		"status":  500.0,
+		"error":   "Internal Server Error",
+		"message": "looks like something went wrong",
+	}
+
+	e := bastion.Tester(t, app)
+	e.PUT("/").WithJSON(body).Expect().
+		Status(http.StatusInternalServerError).
+		JSON().Object().Equal(response)
+}
+
+func TestUpdatingCaptureFailBadRequest(t *testing.T) {
+	t.Parallel()
+	tt := []struct {
+		name     string
+		payload  map[string]interface{}
+		response map[string]interface{}
+	}{
+		{
+			name: "bad request, missing lng",
+			payload: map[string]interface{}{
+				"location": map[string]float64{"lat": 1},
+			},
+			response: map[string]interface{}{
+				"status":  400.0,
+				"error":   "Bad Request",
+				"message": "longitude must not be blank",
+			},
+		},
+		{
+			name: "bad request, missing lat",
+			payload: map[string]interface{}{
+				"location": map[string]float64{"lat": 1000, "lng": 1},
+			},
+			response: map[string]interface{}{
+				"status":  400.0,
+				"error":   "Bad Request",
+				"message": "latitude out of boundaries, may range from -90.0 to 90.0",
+			},
+		},
+	}
+
+	s := &mockUpdatingCaptureService{}
+	app := setupUpdatingCaptureHandler(s, withCaptureMiddle(defaultCapture))
+	e := bastion.Tester(t, app)
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			e.PUT("/").
+				WithJSON(tc.payload).
+				Expect().
+				Status(http.StatusBadRequest).
+				JSON().Object().Equal(tc.response)
+		})
+	}
+}
--- pkg/updating/updating_capture_service_test.go
+++ pkg/updating/updating_capture_service_test.go
@@ -1,244 +1,151 @@
 package updating_test
 
-//func TestUpdateCapture(t *testing.T) {
-//	app, teardown := setup(t)
-//	defer teardown()
-//
-//	e := bastion.Tester(t, app)
-//	capPayload := map[string]interface{}{
-//		"location": map[string]float64{
-//			"lat":       1,
-//			"lng":       12,
-//			"elevation": 50,
-//		},
-//		"timestamp": "1989-12-26T06:01:00Z",
-//		"tags":      []string{},
-//		"payload": []map[string]interface{}{
-//			{
-//				"name":  "power",
-//				"value": []interface{}{-70.0, -100.1, 3.1},
-//			},
-//		},
-//	}
-//
-//	tt := []struct {
-//		name          string
-//		updatePayload map[string]interface{}
-//	}{
-//		{
-//			"update lat",
-//			map[string]interface{}{
-//				"location": map[string]float64{
-//					"lat":       1,
-//					"lng":       capPayload["location"].(map[string]float64)["lng"],
-//					"elevation": capPayload["location"].(map[string]float64)["elevation"],
-//				},
-//				"elevation": capPayload["elevation"],
-//				"timestamp": capPayload["timestamp"],
-//				"tags":      capPayload["tags"],
-//				"payload":   capPayload["payload"],
-//			},
-//		},
-//		{
-//			"update lng",
-//			map[string]interface{}{
-//				"location": map[string]float64{
-//					"lat":       capPayload["location"].(map[string]float64)["lat"],
-//					"lng":       30,
-//					"elevation": capPayload["location"].(map[string]float64)["elevation"],
-//				},
-//				"timestamp": capPayload["timestamp"],
-//				"tags":      capPayload["tags"],
-//				"payload":   capPayload["payload"],
-//			},
-//		},
-//		{
-//			"update elevation",
-//			map[string]interface{}{
-//				"location": map[string]float64{
-//					"lat":       capPayload["location"].(map[string]float64)["lat"],
-//					"lng":       capPayload["location"].(map[string]float64)["lng"],
-//					"elevation": 100,
-//				},
-//				"timestamp": capPayload["timestamp"],
-//				"tags":      capPayload["tags"],
-//				"payload":   capPayload["payload"],
-//			},
-//		},
-//		{
-//			"update timestamp",
-//			map[string]interface{}{
-//				"location":  capPayload["location"],
-//				"timestamp": "2006-07-12T06:01:00Z",
-//				"tags":      capPayload["tags"],
-//				"payload":   capPayload["payload"],
-//			},
-//		},
-//		{
-//			"update payload",
-//			map[string]interface{}{
-//				"location":  capPayload["location"],
-//				"timestamp": capPayload["timestamp"],
-//				"tags":      capPayload["tags"],
-//				"payload": []map[string]interface{}{
-//					{
-//						"name":  "power",
-//						"value": []interface{}{1},
-//					},
-//				},
-//			},
-//		},
-//		{
-//			"update tags",
-//			map[string]interface{}{
-//				"location":  capPayload["location"],
-//				"timestamp": capPayload["timestamp"],
-//				"payload":   capPayload["payload"],
-//				"tags":      []string{"tag1", "tag2"},
-//			},
-//		},
-//		{
-//			"do not update id",
-//			map[string]interface{}{
-//				"id":        "123",
-//				"location":  capPayload["location"],
-//				"timestamp": capPayload["timestamp"],
-//				"tags":      capPayload["tags"],
-//				"payload":   capPayload["payload"],
-//			},
-//		},
-//	}
-//
-//	for _, tc := range tt {
-//		t.Run(tc.name, func(t *testing.T) {
-//			createdObj := e.POST("/test").WithJSON(capPayload).Expect().
-//				Status(http.StatusCreated).JSON().Object().Raw()
-//
-//			e.GET(fmt.Sprintf("/%v", createdObj["id"])).Expect().Status(http.StatusOK)
-//			tc.updatePayload["id"] = createdObj["id"]
-//
-//			updatedObj := e.PUT(fmt.Sprintf("/%v", createdObj["id"])).WithJSON(tc.updatePayload).Expect().
-//				Status(http.StatusOK).
-//				JSON().Object().
-//				ContainsKey("id").ValueEqual("id", tc.updatePayload["id"]).
-//				ContainsKey("location").ValueEqual("location", tc.updatePayload["location"]).
-//				ContainsKey("timestamp").ValueEqual("timestamp", tc.updatePayload["timestamp"]).
-//				ContainsKey("payload").ValueEqual("payload", tc.updatePayload["payload"]).
-//				ContainsKey("tags").ValueEqual("tags", tc.updatePayload["tags"]).
-//				ContainsKey("createdAt").NotEmpty().
-//				ContainsKey("updatedAt").NotEmpty().
-//				Raw()
-//
-//			// updatedAt from put should be after updatedAt from post
-//			updatedAtFromCreate, err := dateparse.ParseAny(createdObj["updatedAt"].(string))
-//			assert.Nil(t, err)
-//			updatedAtFromUpdate, err := dateparse.ParseAny(updatedObj["updatedAt"].(string))
-//			assert.Nil(t, err)
-//			assert.True(t, updatedAtFromUpdate.After(updatedAtFromCreate))
-//		})
-//	}
-//}
-//
-//func TestUpdateCaptureFailsBadRequest(t *testing.T) {
-//	app, teardown := setup(t)
-//	defer teardown()
-//
-//	e := bastion.Tester(t, app)
-//	capPayload := map[string]interface{}{
-//		"location": map[string]float64{
-//			"lat": 1,
-//			"lng": 12,
-//		},
-//		"timestamp": "1989-12-26T06:01:00Z",
-//		"payload": []map[string]interface{}{
-//			{
-//				"name":  "power",
-//				"value": []interface{}{-70.0, -100.1, 3.1},
-//			},
-//		},
-//	}
-//
-//	tt := []struct {
-//		name          string
-//		updatePayload map[string]interface{}
-//		response      map[string]interface{}
-//	}{
-//		{
-//			"lat out of range",
-//			map[string]interface{}{
-//				"location": map[string]float64{
-//					"lat": 200,
-//					"lng": capPayload["location"].(map[string]float64)["lng"],
-//				},
-//				"timestamp": capPayload["timestamp"],
-//				"payload":   capPayload["payload"],
-//			},
-//			map[string]interface{}{
-//				"status":  400.0,
-//				"error":   "Bad Request",
-//				"message": "latitude out of boundaries, may range from -90.0 to 90.0",
-//			},
-//		},
-//		{
-//			"missing payload",
-//			map[string]interface{}{
-//				"location": map[string]float64{
-//					"lat": capPayload["location"].(map[string]float64)["lat"],
-//					"lng": capPayload["location"].(map[string]float64)["lng"],
-//				},
-//				"timestamp": capPayload["timestamp"],
-//			},
-//			map[string]interface{}{
-//				"status":  400.0,
-//				"error":   "Bad Request",
-//				"message": "payload value must not be blank",
-//			},
-//		},
-//	}
-//
-//	for _, tc := range tt {
-//		t.Run(tc.name, func(t *testing.T) {
-//			createdObj := e.POST("/test").WithJSON(capPayload).Expect().
-//				Status(http.StatusCreated).JSON().Object().Raw()
-//			e.GET(fmt.Sprintf("/%v", createdObj["id"])).Expect().Status(http.StatusOK)
-//			tc.updatePayload["id"] = createdObj["id"]
-//			e.PUT(fmt.Sprintf("/%v", createdObj["id"])).WithJSON(tc.updatePayload).Expect().
-//				Status(http.StatusBadRequest).
-//				JSON().Object().Equal(tc.response)
-//		})
-//	}
-//}
-//
-//func TestUpdateCaptureFailsBadRequestWhenMissingID(t *testing.T) {
-//	app, teardown := setup(t)
-//	defer teardown()
-//
-//	e := bastion.Tester(t, app)
-//	capPayload := map[string]interface{}{
-//		"location": map[string]float64{
-//			"lat": 1,
-//			"lng": 12,
-//		},
-//		"timestamp": "1989-12-26T06:01:00Z",
-//		"payload": []map[string]interface{}{
-//			{
-//				"name":  "power",
-//				"value": []interface{}{-70.0, -100.1, 3.1},
-//			},
-//		},
-//	}
-//
-//	response := map[string]interface{}{
-//		"status":  400.0,
-//		"error":   "Bad Request",
-//		"message": "capture id must not be blank",
-//	}
-//
-//	createdObj := e.POST("/test").WithJSON(capPayload).Expect().
-//		Status(http.StatusCreated).JSON().Object().Raw()
-//	e.GET(fmt.Sprintf("/%v", createdObj["id"])).Expect().Status(http.StatusOK)
-//
-//	e.PUT(fmt.Sprintf("/%v", createdObj["id"])).WithJSON(capPayload).Expect().
-//		Status(http.StatusBadRequest).
-//		JSON().Object().Equal(response)
-//}
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/ifreddyrondon/capture/pkg/domain"
+	"github.com/ifreddyrondon/capture/pkg/updating"
+	"github.com/ifreddyrondon/capture/pkg/validator"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/src-d/go-kallax.v1"
+)
+
+type mockStore struct {
+	err error
+}
+
+func (m *mockStore) Save(*domain.Capture) error { return m.err }
+
+var (
+	defaultCaptureID = kallax.NewULID()
+	defaultCapture   = domain.Capture{
+		ID: defaultCaptureID,
+		Payload: domain.Payload{
+			domain.Metric{Name: "power", Value: 10.0},
+		},
+		Location: &domain.Point{LAT: f2P(1), LNG: f2P(1)},
+		Tags:     []string{},
+	}
+)
+
+func TestServiceUpdateCaptureOK(t *testing.T) {
+	t.Parallel()
+
+	tt := []struct {
+		name     string
+		payl     updating.Capture
+		expected domain.Capture
+	}{
+		{
+			name: "given an empty body should return the same capture",
+			payl: updating.Capture{
+				Payload: &validator.Payload{
+					Payload: []domain.Metric{
+						{Name: "power", Value: 10.0},
+					},
+				},
+			},
+			expected: func() domain.Capture {
+				capt := defaultCapture
+				return capt
+			}(),
+		},
+		{
+			name: "given payload should update only the payload",
+			payl: updating.Capture{
+				Payload: &validator.Payload{
+					Payload: []domain.Metric{
+						{Name: "power", Value: []float64{10.0, 20.0}},
+						{Name: "frequency", Value: 300.0},
+					},
+				},
+			},
+			expected: func() domain.Capture {
+				capt := defaultCapture
+				capt.Payload = []domain.Metric{
+					{Name: "power", Value: []float64{10.0, 20.0}},
+					{Name: "frequency", Value: 300.0},
+				}
+				return capt
+			}(),
+		},
+		{
+			name: "given payload and location should update both",
+			payl: updating.Capture{
+				Payload: &validator.Payload{
+					Payload: []domain.Metric{
+						{Name: "power", Value: []float64{20.0}},
+					},
+				},
+				Location: &validator.GeoLocation{LAT: f2P(3), LNG: f2P(5)},
+			},
+			expected: func() domain.Capture {
+				capt := defaultCapture
+				capt.Payload = []domain.Metric{{Name: "power", Value: []float64{20.0}}}
+				capt.Location = &domain.Point{LAT: f2P(3), LNG: f2P(5)}
+				return capt
+			}(),
+		},
+		{
+			name: "given tags should update them",
+			payl: updating.Capture{
+				Tags: []string{"at night"},
+			},
+			expected: func() domain.Capture {
+				capt := defaultCapture
+				capt.Tags = []string{"at night"}
+				return capt
+			}(),
+		},
+		{
+			name: "given timestamp should update timestamp",
+			payl: updating.Capture{
+				Timestamp: &validator.Timestamp{
+					Date: s2n("1989-12-26T06:01:00.00Z"),
+					Time: s2t("1989-12-26T06:01:00.00Z"),
+				},
+			},
+			expected: func() domain.Capture {
+				capt := defaultCapture
+				capt.Timestamp = *s2t("1989-12-26T06:01:00.00Z")
+				return capt
+			}(),
+		},
+	}
+
+	s := updating.NewCaptureService(&mockStore{})
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			crrTime := time.Now()
+			capt := defaultCapture
+			err := s.Update(tc.payl, &capt)
+			assert.Nil(t, err)
+
+			assert.Equal(t, tc.expected.ID, capt.ID)
+			assert.Equal(t, tc.expected.Payload, capt.Payload)
+			assert.Equal(t, tc.expected.Location, capt.Location)
+			assert.Equal(t, tc.expected.Tags, capt.Tags)
+			assert.NotNil(t, capt.CreatedAt)
+			assert.True(t, capt.UpdatedAt.After(crrTime))
+			assert.Nil(t, capt.DeletedAt)
+		})
+	}
+}
+
+func TestServiceUpdateCaptureErrWhenSaving(t *testing.T) {
+	t.Parallel()
+	s := updating.NewCaptureService(&mockStore{err: errors.New("test")})
+	data := updating.Capture{
+		Payload: &validator.Payload{
+			Payload: []domain.Metric{
+				{Name: "power", Value: 10.0},
+			},
+		},
+	}
+	capt := defaultCapture
+	err := s.Update(data, &capt)
+	assert.EqualError(t, err, fmt.Sprintf("could not update capture %v: test", defaultCaptureID))
+}
